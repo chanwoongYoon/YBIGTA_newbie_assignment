6579a8fa2001b48dbcbf0c09a7c73c9421eafceb
chanwoongYoon <cwyoon9718@gmail.com>
2026-07-09 22:19:09 +0900

    minor test

--- 1(1)-Python/1-graph-traversal/test.py
+++ 1(1)-Python/1-graph-traversal/test.py
@@ -9,16 +9,16 @@ import lib
 3 4
 """
 
-n = 4
+n = 5
 m = 5
-root = 1
+root = 3
 
 graph_test = lib.Graph(n)
 
-graph_test.add_edge(1,3)
+graph_test.add_edge(5,4)
+graph_test.add_edge(5,2)
 graph_test.add_edge(1,2)
-graph_test.add_edge(1,4)
-graph_test.add_edge(2,4)
 graph_test.add_edge(3,4)
+graph_test.add_edge(3,1)
 
-graph_test.search_and_print(1)
+graph_test.search_and_print(root)
