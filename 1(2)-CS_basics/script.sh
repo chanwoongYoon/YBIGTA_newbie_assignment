f2129b9ad839cc125c06409231b8a2309df9bb32
chanwoongYoon <cwyoon9718@gmail.com>
2026-07-13 16:01:37 +0900

    submit 1(2)-CS-basics

--- 1(2)-CS_basics/script.sh
+++ 1(2)-CS_basics/script.sh
@@ -1,10 +1,33 @@
+#!/bin/bash
 
 # anaconda(또는 miniconda)가 존재하지 않을 경우 설치해주세요!
 ## TODO
-
+if command -v conda > /dev/null 2>&1; then
+    echo "이미 미니콘다가 설치되어 있습니다. 설치를 건너뜁니다."
+else 
+    curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
+    bash Miniconda3-latest-MacOSX-arm64.sh -b
+fi
 
 # Conda 환셩 생성 및 활성화
 ## TODO
+echo "conda 초기화중..."
+CONDA_BASE=$(conda info --base)
+source "$CONDA_BASE/etc/profile.d/conda.sh"
+
+echo "가상환경을 생성합니다..."
+if conda info --envs | grep -q -w 'myenv'; then
+    echo "myenv 가상환경이 이미 존재합니다. 생성을 건너뜁니다."
+else
+    conda create -n myenv python=3.11 -y
+fi
+
+echo "가상환경을 활성화합니다..."
+conda activate myenv
+
+echo "현재 파이썬 버전 : "
+python --version
+
 
 ## 건드리지 마세요! ##
 python_env=$(python -c "import sys; print(sys.prefix)")
@@ -17,20 +40,34 @@ fi
 
 # 필요한 패키지 설치
 ## TODO
+echo "필요한 패키지를 설치합니다..."
+pip install mypy
 
-# Submission 폴더 파일 실행
-cd submission || { echo "[INFO] submission 디렉토리로 이동 실패"; exit 1; }
+echo "mypy_log.txt 초기화"
+>mypy_log.txt
 
-for file in *.py; do
+for file in submission/*.py; do
     ## TODO
-
+    prob_num=$(echo "$file" | cut -d'_' -f2 | cut -d'.' -f1)
+    echo "실행 중 : $file (문제번호 : $prob_num)"
+    python "$file" < "input/${prob_num}_input" > "output/${prob_num}_output"
+    if mypy "$file" > /dev/null 2>&1; then
+        echo "$prob_num : Success" >> mypy_log.txt
+    else
+        echo "$prob_num : Fail" >> mypy_log.txt
+    fi
 done
 
-# mypy 테스트 실행 및 mypy_log.txt 저장
-## TODO
+# # conda.yml 파일 생성
+# ## TODO
+echo "conda.yml 초기화"
+>conda.yml
+echo "가상환경 설정을 conda.yml로 내보냅니다..."
+conda env export > conda.yml
 
-# conda.yml 파일 생성
-## TODO
+# # 가상환경 비활성화
+# ## TODO
+echo "가상환경을 종료합니다..."
+conda deactivate
 
-# 가상환경 비활성화
-## TODO
+echo "작업 완료"
